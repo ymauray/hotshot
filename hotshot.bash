a94e4677172f62ca2bbdc1a025026a7eab2a71fb
Yannick Mauray <yannick.mauray@gmail.com>
2021-01-07 01:33:40 +0100

    Added license

--- hotshot.bash
+++ hotshot.bash
@@ -1,5 +1,27 @@
 #! /bin/bash
 
+# Generates the Hotshot Racing championship standings from data.csv
+# Copyright (C) 2021  Yannick Mauray
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+echo "hotshot.bash  Copyright (C) 2021  Yannick Mauray"
+echo "This program comes with ABSOLUTELY NO WARRANTY."
+echo "This is free software, and you are welcome to redistribute it"
+echo "under certain conditions."
+echo ""
+
 if [ ! -f data.csv ]; then
     echo "data.csv not found"
     exit 1
